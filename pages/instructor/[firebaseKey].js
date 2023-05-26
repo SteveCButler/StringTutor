ea78a8851cac0e4dd7d4252d6a91546a538d3fbc
Steve Butler <butlersteve.c@gmail.com>
2023-05-26 17:42:24 -0500

    add code to limit the about text on the instructor profile card

--- pages/instructor/[firebaseKey].js
+++ pages/instructor/[firebaseKey].js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
 import { getInstructor } from '../../api/remoteData';
 
 const InstructorFullProfile = () => {
@@ -9,14 +10,20 @@ const InstructorFullProfile = () => {
 
   useEffect(() => {
     getInstructor(firebaseKey).then(setInstructor);
-  }, []);
+  }, [firebaseKey]);
 
   return (
     <div className=" dark-green p-3 rounded-2 bg-white w-75 mt-4 mx-auto">
-      <h1>{instructor.name}</h1>
-      <p className="fs-3">{instructor.instrument}</p>
+      <div className="d-flex gap-5 mb-3 light-green-bg round-top-left round-top-right text-white">
+        <Image className="round-top-left" src={instructor.image} width="250" height="200" />
+        <div>
+          <h1 className="mt-3">{instructor.name}</h1>
+          <p className="fs-3">{instructor.instrument}</p>
+        </div>
+
+      </div>
       <p className="fs-5 mb-0">About</p>
-      <p className="light-green-bg text-white p-3">{instructor.about}</p>
+      <p className="dark-green p-3">{instructor.about}</p>
     </div>
   );
 };
